6f665e8ac34c1a505b6d09a3a51a1580fcbfccc8
bbxx123 <2787922490@qq.com>
2022-09-19 17:14:17 +0800

    完成增删功能

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: chaichai chaichai@cute.com
+ * @Date: 2022-09-15 09:33:56
+ * @LastEditors: chaichai chaichai@cute.com
+ * @LastEditTime: 2022-09-19 14:48:07
+ * @FilePath: \mysqltest\index.js
+ * @Description: 
+ * 
+ * Copyright (c) 2022 by CQUCC-4-433, All Rights Reserved. 
+ */
 const express = require('express')
 const app = express()
 const connection = require('./model/sql')
@@ -50,7 +60,7 @@ app.post('/add', jsonParser, (req, res, next) => {
     // insert into 表名(字段名1，字段名2,....)  values (值1，值2，....)
     // let sql = 'insert into stu (name , age) values("小丽" , 18)';
     let { user_name, password, root } = req.body
-    console.log(req.body);
+    // console.log(req.body);
     let sql = `insert into stu (user_name,password,root) values('${user_name}','${password}',${root})`
     connection.query(sql, (err, result) => {
         if (err) {
@@ -63,6 +73,63 @@ app.post('/add', jsonParser, (req, res, next) => {
         }
     });
 })
+
+app.post('/addhome', jsonParser, (req, res, next) => {
+    // insert into 表名(字段名1，字段名2,....)  values (值1，值2，....)
+    // let sql = 'insert into stu (name , age) values("小丽" , 18)';
+    console.log(req.body);
+
+    let { name, image_url, birsday, phonenumber, node, parentId } = req.body
+    // console.log(req.body);
+    let sql = `insert into home ( name, image_url, birsday, phonenumber, node, parentId ) values('${name}','${image_url}','${birsday}',${phonenumber},${node},${parentId})`
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log('错误', err)
+        } else {
+            // 做添加，result是一个对象，其中有一个属性affectedRows
+            // 表示本次操作之后，影响的行数 
+            // console.log(result); // result就是查询结果
+            res.json(result)
+        }
+    });
+})
+
+app.post('/changehome', jsonParser, (req, res) => {
+    //通过id修改name和age属性值
+    let { id, childrenStatus } = req.body
+    // console.log(req.body);
+
+    // update 表名 set 字段1=值1, 字段2=值2,...  where 修改条件
+    let sql = `update home set childrenStatus='${childrenStatus}' where id=${id}`
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log('错误', err)
+        } else {
+            // 做添加，result是一个对象，其中有一个属性affectedRows
+            // 表示本次操作之后，影响的行数 
+            // console.log(result); // result就是查询结果
+            res.json(result)
+        }
+    });
+})
+
+app.post('/delhome', jsonParser, (req, res) => {
+    //通过id删除
+    let { id } = req.body
+    // delete  from 表名  where 删除条件
+    let sql = `delete from home where id=${id}`;
+    connection.query(sql, (err, result) => {
+        if (err) {
+            console.log('错误', err)
+        } else {
+            // 做添加，result是一个对象，其中有一个属性affectedRows
+            // 表示本次操作之后，影响的行数 
+            // console.log(result); // result就是查询结果
+            res.json(result)
+        }
+    });
+})
+
 
 
 app.listen(3000, () => {
