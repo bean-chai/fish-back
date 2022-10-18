0ae32cd4786fa971871e9f66fb1302f4fe909e9a
bbxx123 <2787922490@qq.com>
2022-10-18 16:07:27 +0800

    add：新增注册、修改字段

--- model/sql.js
+++ model/sql.js
@@ -1,9 +1,9 @@
 /*
  * @Author: chaichai chaichai@cute.com
  * @Date: 2022-09-15 09:44:55
- * @LastEditors: chaichai chaichai@cute.com
- * @LastEditTime: 2022-09-15 11:32:49
- * @FilePath: \mysqltest\model\sql.js
+ * @LastEditors: fengyuanyao fengyuanyao@fanyu.com
+ * @LastEditTime: 2022-10-18 08:48:11
+ * @FilePath: \webclassBack\webclassBack\model\sql.js
  * @Description: 
  * 
  * Copyright (c) 2022 by CQUCC-4-433, All Rights Reserved. 
@@ -17,28 +17,9 @@ const connection = mysql.createConnection({
     database: 'yizhe'
 })
 
-const createTable = (name, field) => {
-    const sql = `CREATE TABLE IF NOT EXISTS ${name} (${field})`
-    connection.query(sql, err => {
-        if (err) throw err
-        console.log(`数据表${name}创建成功`)
-    });
-}
-
 connection.connect((err) => {
-    // 如果有错误对象，表示连接失败
     if (err) return console.log(`数据库连接失败，错误：${err.stack}`)
-    // 没有错误对象提示连接成功
     console.log('mysql数据库连接成功')
-
-    // createTable(
-    //     'home',
-    //     'id INT(8) AUTO_INCREMENT, name VARCHAR(255), image_url VARCHAR(255), birsday DATE, phonenumber VARCHAR(255), node INT(8), parentId INT(8), childrenStatus TINYINT(4), PRIMARY KEY (id)'
-    // )
-    // createTable(
-    //     'stu',
-    //     'user_id INT(11) AUTO_INCREMENT, user_name VARCHAR(32), password VARCHAR(32), root CHAR(1), PRIMARY KEY (user_id)'
-    // )
 });
 
 module.exports = connection
--- router.js
+++ router.js
@@ -5,49 +5,27 @@ const jsonParser = bodyParser.json()
 
 const router = express.Router()
 
-//登录查询
+//登录
 router.get('/search', (req, res) => {
     let sql = 'select * from user';
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
 })
 
-router.get('/searchhome', (req, res) => {
-    let sql = 'select * from home';
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log('错误', err)
-        } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
-            res.json(result)
-        }
-    });
-})
 
 //注册
 router.post('/add', jsonParser, (req, res, next) => {
-    // insert into 表名(字段名1，字段名2,....)  values (值1，值2，....)
-    // let sql = 'insert into stu (name , age) values("小丽" , 18)';
-    let { username, password } = req.body
-    // console.log(req.body);
-    let sql = `insert into user (username,password) values('${username}','${password}')`
+    let { username, password, email, address, birth } = req.body
+    let sql = `insert into user (username,password, email, address, birth) values('${username}','${password}','${email}','${address}','${birth}')`
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
@@ -60,9 +38,6 @@ router.get('/searchinput', (req, res) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
@@ -71,135 +46,64 @@ router.get('/searchinput', (req, res) => {
 //增加留言
 router.post('/addinput', jsonParser, (req, res, next) => {
     let { inputContent } = req.body
-    // console.log(req.body);
     let sql = `insert into input (inputContent) values('${inputContent}')`
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
 })
 
-router.post('/addhome', jsonParser, (req, res, next) => {
-    // insert into 表名(字段名1，字段名2,....)  values (值1，值2，....)
-    // let sql = 'insert into stu (name , age) values("小丽" , 18)';
-    console.log(req.body);
-
-    let { name, image_url, birsday, phonenumber, node, parentId } = req.body
-    // console.log(req.body);
-    let sql = `insert into home ( name, image_url, birsday, phonenumber, node, parentId ) values('${name}','${image_url}','${birsday}',${phonenumber},${node},${parentId})`
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log('错误', err)
-        } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
-            res.json(result)
-        }
-    });
-})
-
+//修改用户信息
 router.post('/change', jsonParser, (req, res) => {
-    //通过id修改name和age属性值
-    let { id, name, oldpass } = req.body
-    let sql = `update user set username='${name}',password='${oldpass}' where id=${id}`
+    let { id, name, oldpass, email, address, birth } = req.body
+    let sql = `update user set username='${name}',password='${oldpass}',email='${email}',address='${address}',birth='${birth}' where id=${id}`
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
 })
 
+//后台修改用户
 router.post('/changeuser', jsonParser, (req, res) => {
     //通过id修改name和age属性值
-    let { id, username, password, rootId } = req.body
-    let sql = `update user set username='${username}',password='${password}',rootId='${rootId}' where id=${id}`
+    let { id, username, password, rootId, email, address, birth } = req.body
+    let sql = `update user set username='${username}',password='${password}',rootId='${rootId}',email='${email}',address='${address}',birth='${birth}' where id=${id}`
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
 })
 
+//后台删除用户
 router.post('/deluser', jsonParser, (req, res) => {
-    //通过id删除
     let { id } = req.body
-    // delete  from 表名  where 删除条件
     let sql = `delete from user where id=${id}`;
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
 })
+
+//后台删除留言
 router.post('/delinput', jsonParser, (req, res) => {
-    //通过id删除
     let { id } = req.body
-    // delete  from 表名  where 删除条件
     let sql = `delete from input where id=${id}`;
     connection.query(sql, (err, result) => {
         if (err) {
             console.log('错误', err)
         } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
-            res.json(result)
-        }
-    });
-})
-
-router.post('/changeback', jsonParser, (req, res) => {
-    //通过id修改name和age属性值
-    let { userId, userName, passWord, Root } = req.body
-    console.log(req.body);
-    // update 表名 set 字段1=值1, 字段2=值2,...  where 修改条件
-    let sql = `update stu set user_name='${userName}',password='${passWord}',root='${Root}' where user_id=${userId}`
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log('错误', err)
-        } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
-            res.json(result)
-        }
-    });
-})
-
-router.post('/delback', jsonParser, (req, res) => {
-    //通过id删除
-    let { id } = req.body
-    // delete  from 表名  where 删除条件
-    let sql = `delete from stu where user_id=${id}`;
-    connection.query(sql, (err, result) => {
-        if (err) {
-            console.log('错误', err)
-        } else {
-            // 做添加，result是一个对象，其中有一个属性affectedRows
-            // 表示本次操作之后，影响的行数
-            // console.log(result); // result就是查询结果
             res.json(result)
         }
     });
